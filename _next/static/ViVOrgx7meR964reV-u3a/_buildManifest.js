d0a186cc4c17ca82a07c8e5a944f28f9177c3262
ChekwubeEze <ezechekwubeinnocent@gmail.com>
2025-12-03 23:32:15 +0200

    Updates

---
+++ _next/static/ViVOrgx7meR964reV-u3a/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/927-fc15c7c783104573.js","static/chunks/pages/index-171089f3523f34a7.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
