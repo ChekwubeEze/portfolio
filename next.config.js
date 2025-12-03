04be09ded6c8c08a4cc2ae69a8d66206fbada782
ChekwubeEze <ezechekwubeinnocent@gmail.com>
2025-12-03 19:02:51 +0200

    added config
    added config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,22 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+/* @type {import('next').NextConfig} */
+/*const nextConfig = {
   reactStrictMode: true,
 }
 
-module.exports = nextConfig
+/*module.exports = nextConfig*/
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  // If your repo is NOT your-username.github.io (project page),
+  // e.g. https://your-username.github.io/portfolio
+  // then uncomment and set basePath and assetPrefix:
+  //
+  // basePath: '/portfolio',
+  // assetPrefix: '/portfolio/',
+};
+
+export default nextConfig;
