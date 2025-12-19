923f627aadfdeaaf0341f97572ea4c2d05d7f88b
ChekwubeEze <ezechekwubeinnocent@gmail.com>
2025-12-19 23:08:59 +0200

    Updates

---
+++ _next/static/7LoL8mhcStd_w35IGT6gh/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/927-fc15c7c783104573.js","static/chunks/pages/index-49e35a0508f78b6d.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- _next/static/cKybMg8Hq9S4v-8wLdsF1/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/927-fc15c7c783104573.js","static/chunks/pages/index-a7b7824ae2920ade.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
